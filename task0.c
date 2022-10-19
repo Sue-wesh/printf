7ea8ee0ab452b44f25e277ffb97ee855e7f83492
Sue-wesh <shiroweru007@gmail.com>
2022-10-19 11:10:00 -0700

    task0

---
+++ task0.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - produce output according to format
+ * @format: character string
+ *
+ * Return: len at the end
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int len = 0, i = 0;
+
+	va_start(list, format);
+
+	while (*format != '\0')
+	{
+		if (format[i] != '%')
+		{
+			len += putchar(format[i]);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+				{
+					len += putchar(va_arg(list, int));
+					break;
+				}
+				case 's':
+				{
+					len += va_arg(list, int);
+					break;
+				}
+				default:
+					break;
+			}
+			i += 2;
+		}
+	}
+	va_end(list);
+	return (len);
+}
+int main(void)
+{
+	char *format = "Hello Dan";
+	int len = 0;
+
+	while (*format != '\0')
+	{
+		len++;
+		format++;
+	}
+	return (len);
+}
